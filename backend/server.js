6e9539ee1a162329e0dbcdb0031b7a9ec4bad781
Pon157 <Vitechek.208@gmail.com>
2025-10-27 22:00:04 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './вход.html';
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+    }
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(3001, () => {
+    console.log('Frontend server running at http://localhost:3001');
+});
